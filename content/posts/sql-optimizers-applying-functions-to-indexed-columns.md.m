19c57df1061585e933d843ae314542825c87f1c8
veskovujovic <veskovujovic@yahoo.com>
2025-02-05 17:43:10 +0100

    Adds initial header.

---
+++ content/posts/sql-optimizers-applying-functions-to-indexed-columns.md.m
@@ -0,0 +1,14 @@
+---
+title: "Apache Spark: Dataset vs Dataframe - The Tortoise and Hare"
+date: 2025-02-05T15:06:41+02:00
+draft: true
+tags:
+  - database
+  - sql
+  - data-engineering
+  - apache-spark
+cover:
+  image: /posts/sql-optimizers/sql-optimizers-cover.png
+  alt: sql-optimizers
+  caption: sql-optimizers
+---
